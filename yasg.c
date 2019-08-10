29de35b35e2cb1dbe19466ddd43e0e827444e7a0
utlandr <rbel068@aucklanduni.ac.nz>
2019-08-10 22:27:49 +1200

    Added random start points

--- yasg.c
+++ yasg.c
@@ -1,82 +1,21 @@
 #include <stdlib.h>
 #include <ncurses.h>
-#include <string.h>
-
-// I'm gonna be wrong and say that this is how one should setup config
-// global variables for a game
-char* TITLE=" Yet Another Snake Game ";     // Game title
-int SCOREBOARD_LEN = 4;                     // Absolute scoreboard length
-
-int START_X = 0;                            // Main window start x
-int START_Y = 0;                            // Main window start y
-int EDGE = 0;                               // Main window edge char
-int CORN = (int)'*';                        // Main window corner char
-
-int G_EDGE = 0;                             // Game window edge char
-int G_CORN = (int)'*';                      // Game window corner char
-
-struct GAME_SCREEN {
-    WINDOW* main_window;
-    WINDOW* game_window;
-    WINDOW* map_window;
-};
-
-typedef struct SNAKE {
-    int id;
-    WINDOW* body;
-    struct SNAKE *next;
-} snake;
-
-// Create a window
-WINDOW* init_window(int edge, int corn, int start_x, int start_y, int height, int width) {
-    WINDOW* win = newwin(height, width, start_x, start_y);
-    wborder(win, edge, edge, edge, edge, corn, corn, corn, corn);
-    return(win);
-}
-
-// Initialize the game screen 
-struct GAME_SCREEN init_game() {
-    struct GAME_SCREEN g_win;
-    // Generate main screen
-    int x_max, y_max;
-    getmaxyx(stdscr, y_max, x_max);
-    g_win.main_window = init_window(EDGE, CORN, START_X, START_Y, y_max, x_max);
-    
-    // Title
-    char* title = TITLE;
-    int title_pos = (int)((x_max-strlen(TITLE))/2);
-    mvwprintw(g_win.main_window, 0, title_pos, title);
-    wrefresh(g_win.main_window);
-
-    // Generate the game map
-    int x_game_start, y_game_start, game_wid, game_len ;
-    x_game_start = 0;
-    y_game_start = SCOREBOARD_LEN;
-
-    game_wid = x_max;
-    game_len = y_max-y_game_start;
-    
-    // Generate game window
-    g_win.game_window = init_window(G_EDGE, G_CORN, y_game_start, x_game_start, game_len, game_wid);
-    wrefresh(g_win.game_window);
-
-    // Create empty map inside game window
-    g_win.map_window = init_window((int)' ', (int)' ', y_game_start + 1, x_game_start + 1, game_len - 2, game_wid - 2); 
-
-    return(g_win);
-
-}
+#include "yasg.h"
 
 int main(void) {
     initscr(); //set up memory and clear screen
-
+    
+    // Init colors if possible
     if (has_colors() == FALSE) {
         endwin();
         printf("Your terminal does not support color\n");
         exit(1);
+
+    } else {
+        start_color();
+        use_default_colors();
     }
-    start_color();
-    use_default_colors();
+
     curs_set(0);
     cbreak();
     noecho();
@@ -85,14 +24,15 @@ int main(void) {
     // Initialize the windows and scoreboard
     struct GAME_SCREEN g_win = init_game();
     int c, x, y;
-    x = y = 10;
-    
-    init_pair(1, COLOR_RED, COLOR_RED);
+    rand_coords(g_win.map_window, &y, &x);    
+
+    init_pair(1, COLOR_BLACK, COLOR_RED);
     wattron(g_win.map_window,COLOR_PAIR(1));
 
     keypad(g_win.map_window, TRUE);
     nodelay(g_win.map_window, TRUE);
     while(1) {
+        mvwprintw(g_win.map_window, 0, 0, "%i is x and %i is y", x, y);
         wrefresh(g_win.map_window);
         c = wgetch(g_win.map_window);
 		switch(c){
